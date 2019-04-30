a0a2daac46403331e2b0990886046d88a36c5840
Jeffrey Lancaster <jeffrey.lancaster@gmail.com>
2019-04-30 20:05:35 +0000

    add balance.py

---
+++ balance.py
@@ -0,0 +1,36 @@
+# 1. Create a notification method called getBalance() that takes an input of the balance and returns a message to the user.
+
+
+
+# 2. Create a deposit method called makeDeposit() that takes two inputs - the current balance and the deposit amount and returns the resulting account balance.
+
+
+
+# 3. Update your makeDeposit() method using your getBalance() method so it returns a message instead of just a number.
+
+
+
+# 4. Create a makeWithdrawal() method that takes two inputs - the current balance and the withdrawal amount and returns the resulting account balance.
+
+
+
+# 5. Update your makeWithdrawal() method using your getBalance() method so it returns a message instead of just a number.
+
+
+
+# 6. Create a method overdraft() that takes an account balance as an input and returns a notice to a user if their account is overdrawn (negative).
+
+
+
+# 7. Include the overdraft() method in your makeWithdrawal() method so makeWithdrawal() returns a message if the result of the withdrawal is a negative account balance.
+
+
+
+# 8. Modify your overdraft() method to indicate to a user by how much their account is overdrawn.
+
+
+
+# 9. Some banks charge a fee whenever a user overdraws their account. Update your makeWithdrawal() method to assess a $25 fee whenever an account is overdrawn.
+
+
+
